9b8ed7ccc2b752dc1b1bd7d8d7087c594f7a3631
ileitao <ignacio.leitao@gmail.com>
2018-12-05 23:55:04 -0300

    Base react project

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// stateless component
+const Footer = () =>  {
+  return (
+    <div className="App">
+    	My footer
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+      	My header
+      </div>
+    );
+  }
+}
+
+export default Header;
